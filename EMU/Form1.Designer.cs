1fc9c3ec05585d294508b125400a6d26b4e7836e
Assaduzzaman Imran <imuk34390@gmail.com>
2025-12-25 15:17:35 +0600

    i do login page and connect database

--- EMU/Form1.Designer.cs
+++ EMU/Form1.Designer.cs
@@ -29,20 +29,222 @@ namespace CarRentalManagementSystem
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PBforLoginPage = new System.Windows.Forms.PictureBox();
+            this.panel1forCross = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoginText = new System.Windows.Forms.Label();
+            this.CrossButtom = new System.Windows.Forms.PictureBox();
+            this.PBmanLogo = new System.Windows.Forms.PictureBox();
+            this.ManlogoHolderPanel = new System.Windows.Forms.Panel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.Clearbtnn = new System.Windows.Forms.Button();
+            this.Loginbtn = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.Passcode = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PBforLoginPage)).BeginInit();
+            this.panel1forCross.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CrossButtom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBmanLogo)).BeginInit();
+            this.ManlogoHolderPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form2
+            // PBforLoginPage
+            // 
+            this.PBforLoginPage.Image = ((System.Drawing.Image)(resources.GetObject("PBforLoginPage.Image")));
+            this.PBforLoginPage.Location = new System.Drawing.Point(-69, -54);
+            this.PBforLoginPage.Name = "PBforLoginPage";
+            this.PBforLoginPage.Size = new System.Drawing.Size(794, 519);
+            this.PBforLoginPage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBforLoginPage.TabIndex = 5;
+            this.PBforLoginPage.TabStop = false;
+            // 
+            // panel1forCross
+            // 
+            this.panel1forCross.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1forCross.Controls.Add(this.pictureBox1);
+            this.panel1forCross.Controls.Add(this.LoginText);
+            this.panel1forCross.Controls.Add(this.CrossButtom);
+            this.panel1forCross.Location = new System.Drawing.Point(0, 0);
+            this.panel1forCross.Name = "panel1forCross";
+            this.panel1forCross.Size = new System.Drawing.Size(647, 44);
+            this.panel1forCross.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 44);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginText
+            // 
+            this.LoginText.AutoSize = true;
+            this.LoginText.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginText.Location = new System.Drawing.Point(57, 9);
+            this.LoginText.Name = "LoginText";
+            this.LoginText.Size = new System.Drawing.Size(51, 23);
+            this.LoginText.TabIndex = 1;
+            this.LoginText.Text = "Login";
+            // 
+            // CrossButtom
+            // 
+            this.CrossButtom.Image = ((System.Drawing.Image)(resources.GetObject("CrossButtom.Image")));
+            this.CrossButtom.Location = new System.Drawing.Point(603, 0);
+            this.CrossButtom.Name = "CrossButtom";
+            this.CrossButtom.Size = new System.Drawing.Size(44, 43);
+            this.CrossButtom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CrossButtom.TabIndex = 0;
+            this.CrossButtom.TabStop = false;
+            this.CrossButtom.Click += new System.EventHandler(this.pictureBox1_Click_2);
+            // 
+            // PBmanLogo
+            // 
+            this.PBmanLogo.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PBmanLogo.Image = ((System.Drawing.Image)(resources.GetObject("PBmanLogo.Image")));
+            this.PBmanLogo.Location = new System.Drawing.Point(0, 0);
+            this.PBmanLogo.Name = "PBmanLogo";
+            this.PBmanLogo.Size = new System.Drawing.Size(123, 155);
+            this.PBmanLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PBmanLogo.TabIndex = 7;
+            this.PBmanLogo.TabStop = false;
+            this.PBmanLogo.Click += new System.EventHandler(this.pictureBox1_Click_3);
+            // 
+            // ManlogoHolderPanel
+            // 
+            this.ManlogoHolderPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ManlogoHolderPanel.Controls.Add(this.txtPassword);
+            this.ManlogoHolderPanel.Controls.Add(this.Clearbtnn);
+            this.ManlogoHolderPanel.Controls.Add(this.Loginbtn);
+            this.ManlogoHolderPanel.Controls.Add(this.txtUsername);
+            this.ManlogoHolderPanel.Controls.Add(this.Passcode);
+            this.ManlogoHolderPanel.Controls.Add(this.Username);
+            this.ManlogoHolderPanel.Controls.Add(this.PBmanLogo);
+            this.ManlogoHolderPanel.Location = new System.Drawing.Point(44, 137);
+            this.ManlogoHolderPanel.Name = "ManlogoHolderPanel";
+            this.ManlogoHolderPanel.Size = new System.Drawing.Size(567, 155);
+            this.ManlogoHolderPanel.TabIndex = 8;
+            this.ManlogoHolderPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(359, 67);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(191, 28);
+            this.txtPassword.TabIndex = 15;
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Clearbtnn
+            // 
+            this.Clearbtnn.BackColor = System.Drawing.Color.IndianRed;
+            this.Clearbtnn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clearbtnn.Location = new System.Drawing.Point(394, 118);
+            this.Clearbtnn.Name = "Clearbtnn";
+            this.Clearbtnn.Size = new System.Drawing.Size(75, 34);
+            this.Clearbtnn.TabIndex = 14;
+            this.Clearbtnn.Text = "Clear";
+            this.Clearbtnn.UseVisualStyleBackColor = false;
+            this.Clearbtnn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Loginbtn
+            // 
+            this.Loginbtn.BackColor = System.Drawing.Color.YellowGreen;
+            this.Loginbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Loginbtn.Location = new System.Drawing.Point(475, 118);
+            this.Loginbtn.Name = "Loginbtn";
+            this.Loginbtn.Size = new System.Drawing.Size(75, 34);
+            this.Loginbtn.TabIndex = 13;
+            this.Loginbtn.Text = "Login";
+            this.Loginbtn.UseVisualStyleBackColor = false;
+            this.Loginbtn.Click += new System.EventHandler(this.Loginbtn_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(359, 26);
+            this.txtUsername.Multiline = true;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(191, 28);
+            this.txtUsername.TabIndex = 12;
+            this.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtUsername.TextChanged += new System.EventHandler(this.UsernameTextBox_TextChanged);
+            // 
+            // Passcode
+            // 
+            this.Passcode.AutoSize = true;
+            this.Passcode.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Passcode.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.Passcode.Location = new System.Drawing.Point(258, 69);
+            this.Passcode.Name = "Passcode";
+            this.Passcode.Size = new System.Drawing.Size(86, 23);
+            this.Passcode.TabIndex = 9;
+            this.Passcode.Text = "Password";
+            this.Passcode.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.Username.Location = new System.Drawing.Point(256, 26);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(89, 23);
+            this.Username.TabIndex = 8;
+            this.Username.Text = "Username";
+            this.Username.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form2";
-            this.Text = "Form2";
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(647, 389);
+            this.Controls.Add(this.ManlogoHolderPanel);
+            this.Controls.Add(this.panel1forCross);
+            this.Controls.Add(this.PBforLoginPage);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
             this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PBforLoginPage)).EndInit();
+            this.panel1forCross.ResumeLayout(false);
+            this.panel1forCross.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CrossButtom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBmanLogo)).EndInit();
+            this.ManlogoHolderPanel.ResumeLayout(false);
+            this.ManlogoHolderPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox PBforLoginPage;
+        private System.Windows.Forms.Panel panel1forCross;
+        private System.Windows.Forms.PictureBox CrossButtom;
+        private System.Windows.Forms.PictureBox PBmanLogo;
+        private System.Windows.Forms.Panel ManlogoHolderPanel;
+        private System.Windows.Forms.Label Passcode;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button Clearbtnn;
+        private System.Windows.Forms.Button Loginbtn;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label LoginText;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- EMU/Form1.cs
+++ EMU/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+//using System.Data;
 
 namespace CarRentalManagementSystem
 {
@@ -20,6 +22,87 @@ namespace CarRentalManagementSystem
         private void Form2_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_2(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsernameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtUsername.Clear();
+            txtPassword.Clear();
+
+            
+            txtUsername.Focus();
+        }
+
+        private void Loginbtn_Click(object sender, EventArgs e)
+        {
+            // EMRAAN হলো আপনার সঠিক সার্ভার নেম
+            SqlConnection con = new SqlConnection(@"Data Source=EMRAAN;Initial Catalog=CarRentalManagementSystem;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            SqlCommand cmd = new SqlCommand("select UserName,Password from logintab where UserName='"
+                + txtUsername.Text + "' and Password='" + txtPassword.Text + "'", con);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Login Success");
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login please check username and password");
+            }
+
+            con.Close();
         }
     }
 }
